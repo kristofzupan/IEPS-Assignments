27ff385fdb4155a737d90bb2536d04041c8b68e2
Kristof Zupan <kristof.zupan.mmv@gmail.com>
2024-05-05 19:30:15 +0200

    Added xpath solutions for rtv and overstock

--- Programming_assignment_2/run-extraction.py
+++ Programming_assignment_2/run-extraction.py
@@ -3,6 +3,7 @@ import sys
 import time
 import re
 import json
+from lxml import html
 
 import codecs
 from selenium import webdriver
@@ -100,7 +101,7 @@ def regexRTV(s):
 
         json_rtv["content"] = content_string
 
-    print(json.dumps(json_rtv))
+    return json.dumps(json_rtv)
 
 
 def regexOverstock(s):
@@ -235,8 +236,108 @@ def runRegEx():
                 print(regexEnaa(s))
 
 
+def xPathRTV(s):
+    author_pattern = "//div[@class='author-timestamp']/strong/text()"
+    time_pattern = '//div[@class="author-timestamp"]/text()'
+    title_pattern = '//h1/text()'
+    subTitle_pattern = "//div[@class='subtitle']/text()"
+    lead_pattern = "//p[@class='lead']/text()"
+    content_pattern = "//article[@class='article']//p/text()"
+
+    json_rtv = {
+        "author": "",
+        "time": "",
+        "title": "",
+        "subTitle": "",
+        "lead": "",
+        "content": ""
+    }
+
+    tree = html.fromstring(s)
+    author = tree.xpath(author_pattern)
+    timestamp = tree.xpath(time_pattern)
+    title = tree.xpath(title_pattern)
+    subTitle = tree.xpath(subTitle_pattern)
+    lead = tree.xpath(lead_pattern)
+    content = tree.xpath(content_pattern)
+
+    if author:
+        json_rtv["author"] = author[0]
+
+    if time:
+        json_rtv["time"] = timestamp[1].split("|")[1].strip()
+
+    if title:
+        json_rtv["title"] = title[0]
+
+    if subTitle:
+        json_rtv["subTitle"] = subTitle[0]
+
+    if lead:
+        json_rtv["lead"] = lead[0]
+
+    if content:
+        merged_content = " ".join(content)
+        json_rtv["content"] = merged_content
+
+    return json.dumps(json_rtv)
+
+
+def xPathOverstock(s):
+    title_pattern = "//tbody//tr[@bgcolor]//td[@valign]//a/b/text()"
+    list_price_pattern = "//tbody//tr[@bgcolor]//td//table//tbody//tr//td//table//tbody//tr//td//s/text()"
+    price_pattern = "//tbody//tr[@bgcolor]//td//table//tbody//tr//td//table//tbody//tr//td//span/b/text()"
+    saving_pattern = "//tbody//tr[@bgcolor]//td//table//tbody//tr//td//table//tbody//tr//td//span[@class='littleorange']/text()"
+    content_pattern = "//tbody//tr[@bgcolor]//td//table//tbody//tr//td/span[@class='normal']/text()"
+
+    tree = html.fromstring(s)
+
+    titles = tree.xpath(title_pattern)
+    list_price = tree.xpath(list_price_pattern)
+    price = tree.xpath(price_pattern)
+    savings = tree.xpath(saving_pattern)
+    content = tree.xpath(content_pattern)
+
+    json_overstock = {"products": []}
+
+    for i in range(0, len(titles)):
+        pattern_saving = r'\$[\d,.]+|\d+%'
+        saving_matches = re.findall(pattern_saving, savings[i])
+
+        json_overstock["products"].append({
+            "title": titles[i],
+            "listPrice": list_price[i],
+            "price": price[i],
+            "saving": saving_matches[0],
+            "saving_percentage": saving_matches[1],
+            "content": content[i]
+        })
+
+    return json.dumps(json_overstock)
+
+
+def xPathEnaa(s):
+    print(s[0:2])
+
+
 def runXPath():
     print('XPath Extraction started')
+
+    for pagePath in renderedPages:
+        print('\n')
+        print("__________________________________________________________________________________________"+pagePath+"__________________________________________________________________________________________")
+        print('\n')
+        with codecs.open(ROOT_DIR + pagePath, "r", "utf-8") as f:
+            s = f.read()
+
+            if pagePath[18:27] == "rtvslo.si":
+                print(xPathRTV(s))
+
+            if pagePath[18:27] == "overstock":
+                print(xPathOverstock(s))
+
+            if pagePath[18:27] == "_enaa.com":
+                print(xPathEnaa(s))
 
 
 def runRoadRunner():
